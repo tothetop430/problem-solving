01f1e93e49587859ff482f7635cd176c3534e378
tothetop430 <jhuseyrer355545@gmail.com>
2022-01-16 13:43:41 +0530

    create HollowDiamondPattern

---
+++ Patterns/src/HollowDiamondPattern.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class HollowDiamondPattern {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Value of N: ");
+        int n = scanner.nextInt(); scanner.nextLine();
+
+        printPattern(n);
+    }
+
+    public static void printPattern(int n) {
+        int i,j;
+        for (i=0; i<n; i++) {
+            for(j=i; j<n; j++) {
+                if (j==n-1) System.out.print("*");
+                else System.out.print("* ");
+            }
+            for(j=0; j<2*i+1; j++) {
+                System.out.print("  ");
+            }
+            for(j=i; j<n; j++) {
+                if (j==n-1) System.out.print("*");
+                else System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (i=0; i<n; i++) {
+            for (j=0; j<=i; j++) {
+                if (j==i) System.out.print("*");
+                else System.out.print("* ");
+            }
+            for (j=2*(n-1); j>=2*i; j--) System.out.print("  ");
+            for (j=0; j<=i; j++) {
+                if (j==i) System.out.print("*");
+                else System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+
+Enter Value of N:
+5
+* * * * *  * * * * *
+* * * *      * * * *
+* * *          * * *
+* *              * *
+*                  *
+*                  *
+* *              * *
+* * *          * * *
+* * * *      * * * *
+* * * * *  * * * * *
+
+ */
