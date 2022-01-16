9de488412c0927e6163befdfc19d57116bdaf028
tothetop430 <jhuseyrer355545@gmail.com>
2022-01-16 13:05:49 +0530

    create TriangleStarPatternSingleLoop

---
+++ Patterns/src/TriangleStarPatternSingleLoop.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class TriangleStarPatternSingleLoop {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Value of N: ");
+        int n = scanner.nextInt(); scanner.nextLine();
+
+        printPattern(n);
+    }
+
+    public static void printPattern(int n) {
+        int i, k, flag = 1;
+        for (i=1, k=0; i<=2*n-1; i++) {
+            if (i < n-k) System.out.print(" ");
+            else {
+                if (flag == 1) System.out.print("*");
+                else System.out.print(" ");
+                flag = 1 - flag;
+            }
+            if (i == (n+k)) {
+                k++;
+                System.out.println();
+
+                if (i == 2 * n - 1) break;
+
+                i = 0;
+                flag = 1;
+            }
+        }
+    }
+}
