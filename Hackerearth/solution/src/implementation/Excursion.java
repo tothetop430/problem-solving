5f171def61b53f03f619f8b0cf650e2e01758aaf
tothetop430 <jhuseyrer355545@gmail.com>
2022-04-17 15:26:03 +0530

    solve Excursion

---
+++ Hackerearth/solution/src/implementation/Excursion.java
@@ -0,0 +1,20 @@
+// https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/algorithm/excursion-2d080f3a/
+
+package implementation;
+
+import java.util.Scanner;
+
+public class Excursion {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int test = scanner.nextInt();
+        while(test--!=0) {
+            double n = scanner.nextInt();
+            double m = scanner.nextInt();
+            double k = scanner.nextInt();
+            System.out.println((int)(Math.ceil(n / k) + Math.ceil(m / k)));
+        }
+    }
+
+}
