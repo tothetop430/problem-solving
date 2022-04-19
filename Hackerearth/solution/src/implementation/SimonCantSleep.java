99351cff8e69a466be9832bcdf3a469d64e09842
tothetop430 <jhuseyrer355545@gmail.com>
2022-04-19 21:44:52 +0530

    solve SimonCantSleep

---
+++ Hackerearth/solution/src/implementation/SimonCantSleep.java
@@ -0,0 +1,27 @@
+// https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/algorithm/simon-cant-sleep-3beb3241/
+
+package implementation;
+
+import java.util.Scanner;
+
+public class SimonCantSleep {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] hrsMins = scanner.nextLine().split(":");
+        int hrs = Integer.parseInt(hrsMins[0]);
+        int mins = Integer.parseInt(hrsMins[1]);
+        int count = 0;
+        if (hrs >= 12) {
+            count = 11;
+            hrs -= 12;
+        }
+        count += hrs;
+        double hrHandDeg = ((hrs % 12) * 30) + (mins * (1.0/2.0));
+        double minHandDeg = (double) mins * 6;
+//        System.out.println(hrHandDeg + " " + minHandDeg);
+        if (minHandDeg >= hrHandDeg) count++;
+        System.out.println(count);
+    }
+
+}
