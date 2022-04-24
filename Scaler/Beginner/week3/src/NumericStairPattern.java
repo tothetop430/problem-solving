c120398ec88b00989800732b39ac133aa9e10eab
tothetop430 <jhuseyrer355545@gmail.com>
2022-04-24 17:59:13 +0530

    solve NumericStairPattern

---
+++ Scaler/Beginner/week3/src/NumericStairPattern.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class NumericStairPattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int i = 1;
+        while(i <= N) {
+            int j = 1;
+            while(j <= i) {
+                System.out.print(j);
+                if(j != i) System.out.print(" ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
