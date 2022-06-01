e7d30480968be847b1ece9c93eb1b8d392f816bf
tothetop430 <jhuseyrer355545@gmail.com>
2022-06-01 19:14:00 +0530

    update files

--- Scaler/Beginner/src.com.javapractice/week2/src/CheckEvenOdd.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class CheckEvenOdd {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(scanner.nextInt() % 2);
-        scanner.close();
-    }
-}
