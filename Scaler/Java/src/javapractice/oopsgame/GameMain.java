6e652fe0a62b9ff02162db58a91360323d7be8fe
tothetop430 <jhuseyrer355545@gmail.com>
2022-06-04 23:14:45 +0530

    create GameMain

---
+++ Scaler/Java/src/javapractice/oopsgame/GameMain.java
@@ -0,0 +1,10 @@
+package javapractice.oopsgame;
+
+public class GameMain {
+
+    public static void main(String[] args) {
+        Game game1 = new Game("Siddharth", "Abhishek", "Tanuja");
+        game1.startGame();
+    }
+
+}
