88497039cbb455e7ba21052a5a4889f480508d3e
tothetop430 <jhuseyrer355545@gmail.com>
2022-06-09 13:57:03 +0530

    solve week6 functions problems

---
+++ Scaler/Java/src/practiceproblems/week6/functions/AreaOfCircle.java
@@ -0,0 +1,9 @@
+package practiceproblems.week6.functions;
+
+public class AreaOfCircle {
+
+    public int solve(int A) {
+        return (int) Math.ceil(Math.PI * A * A);
+    }
+
+}
---
+++ Scaler/Java/src/practiceproblems/week6/functions/VolumeOfSphere.java
@@ -0,0 +1,9 @@
+package practiceproblems.week6.functions;
+
+public class VolumeOfSphere {
+
+    public int solve(final int A) {
+        return (int) Math.ceil((4 * Math.PI * A * A * A) / 3);
+    }
+
+}
