c45669ad96fae6afaa0334a4b71c4ac029d9729a
tothetop430 <jhuseyrer355545@gmail.com>
2022-06-14 18:25:00 +0530

    solve week8 string problems

---
+++ Scaler/Java/src/practiceproblems/week8/strings/FirstOccurence.java
@@ -0,0 +1,10 @@
+package practiceproblems.week8.strings;
+
+public class FirstOccurence {
+
+    public int solve(final String A, final int B) {
+        char chr = (char) B;
+        return A.indexOf(chr);
+    }
+
+}
