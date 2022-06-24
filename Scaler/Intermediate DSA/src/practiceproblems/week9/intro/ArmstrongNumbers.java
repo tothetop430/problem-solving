3363b0fabe042457110732e44e8ef4911d5f503d
tothetop430 <jhuseyrer355545@gmail.com>
2022-06-24 21:42:54 +0530

    solve week9 intro problems

---
+++ Scaler/Intermediate DSA/src/practiceproblems/week9/intro/ArmstrongNumbers.java
@@ -0,0 +1,29 @@
+package practiceproblems.week9.intro;
+
+import java.util.Scanner;
+
+public class ArmstrongNumbers {
+
+    private static int sumOfCubes(int n) {
+        int sum = 0;
+        while(n != 0) {
+            int last = n % 10;
+            sum += last * last * last;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    private static boolean isArmstrong(int n) {
+        return sumOfCubes(n) == n;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for(int i=1; i<=n; i++) {
+            if(isArmstrong(i)) System.out.println(i);
+        }
+    }
+
+}
---
+++ Scaler/Intermediate DSA/src/practiceproblems/week9/intro/MultiplicationTable.java
@@ -0,0 +1,15 @@
+package practiceproblems.week9.intro;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for(int i=1; i<=10; i++) {
+            System.out.println(n + " * " + i + " = " + n * i);
+        }
+    }
+
+}
