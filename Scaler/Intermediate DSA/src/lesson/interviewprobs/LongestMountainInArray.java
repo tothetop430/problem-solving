5444d8542c27980917a963f42f6c12c4d97f84b4
tothetop430 <jhuseyrer355545@gmail.com>
2022-07-14 21:41:31 +0530

    solve array interview questions

--- Scaler/Intermediate DSA/src/lesson/interviewprobs/LongestMountainInArray.java
+++ Scaler/Intermediate DSA/src/lesson/interviewprobs/LongestMountainInArray.java
@@ -2,6 +2,26 @@ package lesson.interviewprobs;
 
 public class LongestMountainInArray {
 
-
+    public int longestMountain(int[] arr) {
+        int n = arr.length;
+        int largest = 0;
+        for(int i=1; i<n-1; ) {
+            int peak = arr[i];
+            int count = 1;
+            if(arr[i-1] < peak && arr[i+1] < peak) {
+                int j = i-1;
+                while(j>=0 && arr[j] < arr[j+1]) {
+                    count++; j--;
+                }
+                while(i<n-1 && arr[i] > arr[i+1]) {
+                    count++; i++;
+                }
+                largest = Math.max(largest, count);
+            }
+            else i++;
+        }
+        if(largest == 1) largest = 0;
+        return largest;
+    }
 
 }
