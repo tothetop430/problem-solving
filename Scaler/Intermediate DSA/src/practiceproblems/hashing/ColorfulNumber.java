450da43c4e1122d75149b3ee76798322bec80117
tothetop430 <jhuseyrer355545@gmail.com>
2022-09-13 15:41:46 +0530

    solve hashing questions

---
+++ Scaler/Intermediate DSA/src/practiceproblems/hashing/ColorfulNumber.java
@@ -0,0 +1,31 @@
+package practiceproblems.hashing;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class ColorfulNumber {
+
+    public int colorful(int A) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        while(A>0){
+            int a = A%10;
+            arr.add(a);
+            A /= 10;
+        }
+
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int i=0; i< arr.size(); i++){
+            int prod = 1;
+            for(int j = i; j < arr.size(); j++){
+                prod *= arr.get(j);
+                if(set.contains(prod)){
+                    return 0;
+                }else{
+                    set.add(prod);
+                }
+            }
+        }
+        return 1;
+    }
+
+}
