1ab451f7a1cf84c3662b1948b679da395a638b5a
tothetop430 <jhuseyrer355545@gmail.com>
2022-10-14 19:31:44 +0530

    solve bit manipulation questions

---
+++ Scaler/Advanced DSA/src/bitmanipulation/MinXORPair.java
@@ -0,0 +1,18 @@
+package src.bitmanipulation;
+
+import java.util.Arrays;
+
+public class MinXORPair {
+
+    public int findMinXor(int[] A) {
+        Arrays.sort(A);
+        int ans = Integer.MAX_VALUE;
+
+        for(int i=1; i<A.length; i++) {
+            int xor = A[i-1] ^ A[i];
+            ans = Integer.min(ans, xor);
+        }
+        return ans;
+    }
+
+}
