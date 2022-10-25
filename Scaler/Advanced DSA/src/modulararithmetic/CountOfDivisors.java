783053d0a55138c7c005b06f514366a23c4174ce
tothetop430 <jhuseyrer355545@gmail.com>
2022-10-25 17:08:01 +0530

    solve modular arithmetic questions

---
+++ Scaler/Advanced DSA/src/modulararithmetic/CountOfDivisors.java
@@ -0,0 +1,22 @@
+package src.modulararithmetic;
+
+public class CountOfDivisors {
+
+    public int noOfDiv(int num) {
+        int count = 0;
+        for(int i=1; i * i <= num; i++) {
+            if(i * i == num) count++;
+            else if (num % i == 0) count += 2;
+        }
+        return count;
+    }
+
+    public int[] solve(int[] A) {
+        int[] count = new int[A.length];
+        for(int i=0; i<A.length; i++) {
+            count[i] = noOfDiv(A[i]);
+        }
+        return count;
+    }
+
+}
