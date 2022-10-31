97c75c59c33579473fa39ba1fc34c41de0db5c60
tothetop430 <jhuseyrer355545@gmail.com>
2022-10-31 15:05:59 +0530

    solve sorting questions

---
+++ Scaler/Advanced DSA/src/sorting/BubbleSort.java
@@ -0,0 +1,25 @@
+package src.sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(bubblesort(new int[]{5, 4, 3, 2, 1, 0, 0, 1, 4, 6, 5})));
+    }
+
+
+    public static int[] bubblesort(int[] arr) {
+        for(int i=0; i<arr.length; i++) {
+            for(int j=0; j<arr.length - i - 1; j++) {
+                if(arr[j+1] < arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+}
