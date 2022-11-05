8b3cccecde0bdad6e47e5c66a370ad6defc46cc7
tothetop430 <jhuseyrer355545@gmail.com>
2022-11-05 18:14:47 +0530

    solve sorting questions

--- Scaler/Advanced DSA/src/sorting/one/InsertionSort.java
+++ Scaler/Advanced DSA/src/sorting/one/InsertionSort.java
@@ -8,7 +8,6 @@ public class InsertionSort {
         System.out.println(Arrays.toString(insertionsort(new int[] {5, 4, 3, 2, 1, 0, 1, 0, 4, 6, 5})));
         System.out.println(Arrays.toString(insertionsort(new int[] {1,2,3,4,5})));
     }
-
     public static int[] insertionsort(int[] arr) {
         for(int i=1; i<arr.length; i++) {
             int j = i-1;
