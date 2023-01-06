751219336793855b9f5b59e4ff2f52058e065f6f
tothetop430 <jhuseyrer355545@gmail.com>
2023-01-06 13:22:20 +0530

    solve hashing questions

--- Scaler/Advanced DSA/src/hashing/ColorfulNumber.java
+++ Scaler/Advanced DSA/src/hashing/ColorfulNumber.java
@@ -12,7 +12,6 @@ public class ColorfulNumber {
             arr.add(a);
             A /= 10;
         }
-
         HashSet<Integer> set = new HashSet<>();
         for(int i=0; i< arr.size(); i++){
             int prod = 1;
