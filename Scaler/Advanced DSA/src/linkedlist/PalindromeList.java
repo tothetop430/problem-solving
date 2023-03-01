6d479925f12351bb62e054584b47b8f8bf0eec5f
tothetop430 <jhuseyrer355545@gmail.com>
2023-03-01 11:33:40 +0530

    solve linked list questions

--- Scaler/Advanced DSA/src/linkedlist/PalindromeList.java
+++ Scaler/Advanced DSA/src/linkedlist/PalindromeList.java
@@ -25,7 +25,7 @@ public class PalindromeList {
         ListNode slow = head;
         ListNode fast = head.next;
 
-        if(fast != null && fast.next != null) {
+        while(fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
