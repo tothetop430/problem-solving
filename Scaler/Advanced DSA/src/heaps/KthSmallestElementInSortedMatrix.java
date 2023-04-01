66c773f8a5509991158c8ea582723506aa3e1429
tothetop430 <jhuseyrer355545@gmail.com>
2023-04-01 15:00:41 +0530

    solve tries questions

---
+++ Scaler/Advanced DSA/src/heaps/KthSmallestElementInSortedMatrix.java
@@ -0,0 +1,22 @@
+package src.heaps;
+
+import java.util.PriorityQueue;
+
+public class KthSmallestElementInSortedMatrix {
+
+    public int solve(int[][] A, int B) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int i=0; i<A.length; i++) {
+            for(int j=0; j<A[0].length; j++) {
+                minHeap.add(A[i][j]);
+            }
+        }
+        int result = minHeap.peek();
+        while(B != 0) {
+            result = minHeap.poll();
+            B--;
+        }
+        return result;
+    }
+
+}
