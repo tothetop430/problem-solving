4c15b0f1927ffb3a27f5a62acc65457fee4c1a4e
tothetop430 <jhuseyrer355545@gmail.com>
2023-04-01 13:02:17 +0530

    solve tries questions

---
+++ Scaler/Advanced DSA/src/heaps/ConnectRopes.java
@@ -0,0 +1,23 @@
+package src.heaps;
+
+import java.util.PriorityQueue;
+
+public class ConnectRopes {
+
+    public int solve(int[] A) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        int minCost = 0;
+        for(int val: A) {
+            minHeap.add(val);
+        }
+        while(minHeap.size() != 0) {
+            int first = minHeap.poll();
+            int second = 0;
+            if(minHeap.size() != 0) second = minHeap.poll();
+            minCost += first + second;
+            if(minHeap.size() != 0) minHeap.add(first + second);
+        }
+        return minCost;
+    }
+
+}
