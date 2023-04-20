6888ba297ea4ece1e10380f70f5b99c317a6b577
tothetop430 <jhuseyrer355545@gmail.com>
2023-04-20 11:39:45 +0530

    solve leetcode 75 problem

---
+++ LeetCode/src/IsSubsequence_392.java
@@ -0,0 +1,20 @@
+package src;
+
+public class IsSubsequence_392 {
+
+    public boolean isSubsequence(String s, String t) {
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+        if(i > j) return false;
+
+        while(i>=0 && j>=0) {
+            if(s.charAt(i) == t.charAt(j)) {
+                i--;
+            }
+            j--;
+        }
+
+        return i < 0;
+    }
+
+}
