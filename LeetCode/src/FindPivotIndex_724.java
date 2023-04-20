93c997fc5ecc04904ee36565924d932940abac83
tothetop430 <jhuseyrer355545@gmail.com>
2023-04-20 10:48:16 +0530

    solve leetcode 75 problem

---
+++ LeetCode/src/FindPivotIndex_724.java
@@ -0,0 +1,37 @@
+package src;
+
+public class FindPivotIndex_724 {
+    public int[] createPrefixSumArr(int[] arr) {
+        int[] result = new int[arr.length];
+        for(int i=0; i<arr.length; i++) {
+            if(i != 0) {
+                result[i] = result[i-1] + arr[i];
+            }
+            else result[i] = arr[i];
+        }
+        return result;
+    }
+
+    public int pivotIndex(int[] nums) {
+        int[] prefixSumArr = createPrefixSumArr(nums);
+        int leftSum = 0;
+        int rightSum = 0;
+        int pivotIndx = -1;
+        for(int i=0; i<prefixSumArr.length; i++) {
+            if(i != 0) {
+                leftSum = prefixSumArr[i-1];
+            }
+            if(i != prefixSumArr.length - 1) {
+                rightSum = prefixSumArr[prefixSumArr.length - 1] - prefixSumArr[i];
+            }
+            if(i == prefixSumArr.length - 1) {
+                rightSum = 0;
+            }
+            if(leftSum == rightSum) {
+                pivotIndx = i;
+                break;
+            }
+        }
+        return pivotIndx;
+    }
+}
