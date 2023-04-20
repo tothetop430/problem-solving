30b91faca834bc2e2ee48a823440bd81fbdf0e85
tothetop430 <jhuseyrer355545@gmail.com>
2023-04-20 10:35:29 +0530

    solve leetcode 75 problem

---
+++ LeetCode/src/RunningSum1DArray.java
@@ -0,0 +1,14 @@
+package src;
+
+public class RunningSum1DArray {
+
+    public int[] runningSum(int[] nums) {
+        int[] sumArr = new int[nums.length];
+        sumArr[0] = nums[0];
+        for(int i=1; i<nums.length; i++) {
+            sumArr[i] = sumArr[i-1] + nums[i];
+        }
+        return sumArr;
+    }
+
+}
