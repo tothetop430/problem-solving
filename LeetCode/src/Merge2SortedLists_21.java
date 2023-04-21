b132d712a226e6e688a817a68ee89ee57e066bcb
tothetop430 <jhuseyrer355545@gmail.com>
2023-04-21 12:41:29 +0530

    solve leetcode 75 problem

--- LeetCode/src/Merge2SortedLists_21.java
+++ LeetCode/src/Merge2SortedLists_21.java
@@ -1,6 +1,5 @@
 package src;
 
-
 class ListNode {
       int val;
       ListNode next;
@@ -8,12 +7,12 @@ class ListNode {
       ListNode(int val) { this.val = val; }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
-
 public class Merge2SortedLists_21 {
+
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode head = null;
         ListNode ptr = null;
-        ListNode temp;
+        ListNode temp = null;
         while(list1 != null && list2 != null) {
             if(list1.val <= list2.val) {
                 temp = list1;
@@ -59,4 +58,5 @@ public class Merge2SortedLists_21 {
         }
         return head;
     }
+
 }
