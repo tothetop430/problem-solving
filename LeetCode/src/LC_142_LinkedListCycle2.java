7106d9351d159549aaedac23c3092beeff897c9e
tothetop430 <jhuseyrer355545@gmail.com>
2023-04-22 14:19:12 +0530

    solve leetcode problem

---
+++ LeetCode/src/LC_142_LinkedListCycle2.java
@@ -0,0 +1,24 @@
+package src;
+
+public class LC_142_LinkedListCycle2 {
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) break;
+        }
+        if(fast == null || fast.next == null) return null;
+
+        slow = head;
+        while(slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+
+}
