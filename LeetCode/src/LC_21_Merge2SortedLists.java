d3cad23de6f05f1ac1adb371130b00776ed195a3
tothetop430 <jhuseyrer355545@gmail.com>
2023-04-22 14:39:59 +0530

    solve leetcode problem

--- LeetCode/src/LC_21_Merge2SortedLists.java
+++ LeetCode/src/LC_21_Merge2SortedLists.java
@@ -7,56 +7,21 @@ class ListNode {
       ListNode(int val) { this.val = val; }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
-public class Merge2SortedLists_21 {
+
+public class LC_21_Merge2SortedLists {
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode head = null;
-        ListNode ptr = null;
-        ListNode temp = null;
-        while(list1 != null && list2 != null) {
-            if(list1.val <= list2.val) {
-                temp = list1;
-                list1 = list1.next;
-            }
-            else {
-                temp = list2;
-                list2 = list2.next;
-            }
-
-            if(head == null) {
-                head = temp;
-                ptr = head;
-            }
-            else {
-                ptr.next = temp;
-                ptr = ptr.next;
-            }
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+        ListNode head;
+        if(list1.val < list2.val) {
+            head = list1;
+            head.next = mergeTwoLists(list1.next, list2);
         }
-
-        while(list1 != null) {
-            if(head == null) {
-                head = list1;
-                ptr = head;
-            }
-            else {
-                ptr.next = list1;
-                ptr = ptr.next;
-            }
-            list1 = list1.next;
-        }
-
-        while(list2 != null) {
-            if(head == null) {
-                head = list2;
-                ptr = head;
-            }
-            else {
-                ptr.next = list2;
-                ptr = ptr.next;
-            }
-            list2 = list2.next;
+        else {
+            head = list2;
+            head.next = mergeTwoLists(list1, list2.next);
         }
         return head;
     }
-
 }
