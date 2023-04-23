d87b9934f123051bb2418c97ac1e40381a865ff4
tothetop430 <jhuseyrer355545@gmail.com>
2023-04-23 11:50:29 +0530

    solve leetcode problem

---
+++ LeetCode/src/LC_300_LongestIncreasingSubsequence.java
@@ -0,0 +1,22 @@
+package src;
+
+import java.util.Arrays;
+
+public class LC_300_LongestIncreasingSubsequence {
+
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int ans = 1;
+        for(int i=1; i<nums.length; i++) {
+            for(int j=0; j<i; j++) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = Integer.max(dp[i], dp[j]+1);
+                }
+            }
+            ans = Integer.max(ans, dp[i]);
+        }
+        return ans;
+    }
+
+}
