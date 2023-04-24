35c6dbded6d833a399de9537b69406bfea523e95
tothetop430 <jhuseyrer355545@gmail.com>
2023-04-24 11:06:17 +0530

    solve leetcode problem

---
+++ LeetCode/src/LC_589_NAryTreePreorderTraversal.java
@@ -0,0 +1,41 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+public class LC_589_NAryTreePreorderTraversal {
+
+    List<Integer> result;
+    public List<Integer> preorder(Node root) {
+        result = new ArrayList<>();
+        preTraversal(root);
+        return result;
+    }
+
+    public void preTraversal(Node root) {
+        if(root == null) return;
+        result.add(root.val);
+        if(root.children != null) {
+            for(Node child: root.children) {
+                preTraversal(child);
+            }
+        }
+    }
+
+}
