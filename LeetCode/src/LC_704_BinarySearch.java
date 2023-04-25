38bd2369a494fbe4192837243bef4ff5ccee1411
tothetop430 <jhuseyrer355545@gmail.com>
2023-04-25 11:03:42 +0530

    solve leetcode problem

---
+++ LeetCode/src/LC_704_BinarySearch.java
@@ -0,0 +1,25 @@
+package src;
+
+public class LC_704_BinarySearch {
+
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        int ans = -1;
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target) {
+                ans = mid;
+                break;
+            }
+            if(nums[mid] > target) {
+                end = mid - 1;
+            }
+            else if(nums[mid] < target) {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+}
