f5116b1dafe762ee9c08d80eff325d767754af89
tothetop430 <jhuseyrer355545@gmail.com>
2023-04-26 12:07:12 +0530

    solve leetcode problem

--- LeetCode/src/studyplan75/LC_235_LowestCommonAncestorBST.java
+++ LeetCode/src/studyplan75/LC_235_LowestCommonAncestorBST.java
@@ -9,7 +9,7 @@ public class LC_235_LowestCommonAncestorBST {
     public TreeNode lca(TreeNode root, int min, int max) {
         if(root == null) return null;
         if(min <= root.val && root.val <= max) return root;
-        if(min < root.val && root.val > max) return lca(root.left, min, max);
+        if(min < root.val) return lca(root.left, min, max);
         else return lca(root.right, min, max);
     }
 
