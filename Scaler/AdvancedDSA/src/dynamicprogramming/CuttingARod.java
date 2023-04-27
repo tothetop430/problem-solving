145aebe38fee7de2d65c6f5aa2371f1da6858b63
tothetop430 <jhuseyrer355545@gmail.com>
2023-04-27 10:58:08 +0530

    solve dynamic programming questions

---
+++ Scaler/AdvancedDSA/src/dynamicprogramming/CuttingARod.java
@@ -0,0 +1,28 @@
+package src.dynamicprogramming;
+
+import java.util.Arrays;
+
+public class CuttingARod {
+
+    public int solve(int[] A) {
+        int[][] dp = new int[A.length+1][A.length+1];
+        for(int[] arr: dp) {
+            Arrays.fill(arr, -1);
+        }
+        return findMaxCost(A, A.length, A.length, dp);
+    }
+
+    public int findMaxCost(int[] values, int n, int cap, int[][] dp) {
+        if(n == 0 || cap == 0) return 0;
+        if(dp[n][cap] != -1) return dp[n][cap];
+
+        int include = 0;
+        if(n <= cap) {
+            include = values[n-1] + findMaxCost(values, n, cap-n, dp);
+        }
+        int exclude = findMaxCost(values, n-1, cap, dp);
+        dp[n][cap] = Integer.max(include, exclude);
+        return dp[n][cap];
+    }
+
+}
