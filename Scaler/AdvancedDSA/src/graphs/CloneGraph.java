d88d2711ed8a12e5f0609be0ab0e254ca39b6fe6
tothetop430 <jhuseyrer355545@gmail.com>
2023-05-19 14:32:58 +0530

    solve graph questions

--- Scaler/AdvancedDSA/src/graphs/CloneGraph.java
+++ Scaler/AdvancedDSA/src/graphs/CloneGraph.java
@@ -5,7 +5,7 @@ import java.util.*;
 class UndirectedGraphNode {
       int label;
       List<UndirectedGraphNode> neighbors;
-      UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+      UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<>(); }
 }
 
 public class CloneGraph {
