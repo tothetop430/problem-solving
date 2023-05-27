b9bf2032d77e99bedfa2876c9191cb9afebed4a9
tothetop430 <jhuseyrer355545@gmail.com>
2023-05-27 18:42:14 +0530

    solve top interview 150 problem

---
+++ LeetCode/src/topinterview150/LC_169_MajorityElement.java
@@ -0,0 +1,20 @@
+package src.topinterview150;
+
+public class LC_169_MajorityElement {
+
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int majElem = 0;
+        for(int val: nums) {
+            if(count == 0 || majElem == val) {
+                majElem = val;
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+        return majElem;
+    }
+
+}
