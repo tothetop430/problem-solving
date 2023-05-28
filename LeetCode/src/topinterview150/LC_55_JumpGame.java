db54a14067b2332c6fb0eac66aac3476d477ffcd
tothetop430 <jhuseyrer355545@gmail.com>
2023-05-28 15:35:23 +0530

    solve top interview 150 problem

--- LeetCode/src/topinterview150/LC_55_JumpGame.java
+++ LeetCode/src/topinterview150/LC_55_JumpGame.java
@@ -9,7 +9,7 @@ public class LC_55_JumpGame {
         Arrays.fill(dp, -1);
         dp[0] = 1;
         for(int i=0; i<nums.length; i++) {
-            if(dp[i] == -1) continue;
+            if(dp[i] == -1) break;
             int j = i + nums[i];
             if(j >= nums.length) return true;
             while(j >= i && dp[j] == -1) {
@@ -17,7 +17,7 @@ public class LC_55_JumpGame {
                 j--;
             }
         }
-        return dp[nums.length-1] == 1? true: false;
+        return dp[nums.length-1] == 1;
     }
 
 }
