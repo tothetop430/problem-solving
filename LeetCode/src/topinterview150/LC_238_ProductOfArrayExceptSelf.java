66363b2a63a342819c60f30c00719506281d43fa
tothetop430 <jhuseyrer355545@gmail.com>
2023-05-29 12:18:47 +0530

    solve top interview 150 problem

---
+++ LeetCode/src/topinterview150/LC_238_ProductOfArrayExceptSelf.java
@@ -0,0 +1,29 @@
+package src.topinterview150;
+
+public class LC_238_ProductOfArrayExceptSelf {
+
+    public int[] productExceptSelf(int[] nums) {
+        int[] prefix = new int[nums.length];
+        int[] suffix = new int[nums.length];
+        int prev = 1;
+        for(int i=0; i<nums.length; i++) {
+            prefix[i] = prev * nums[i];
+            prev = prefix[i];
+        }
+        prev = 1;
+        for(int i=nums.length-1; i>=0; i--) {
+            suffix[i] = prev * nums[i];
+            prev = suffix[i];
+        }
+
+        int[] result = new int[nums.length];
+        for(int i=0; i<nums.length; i++) {
+            if(i == 0) result[i] = suffix[i+1];
+            else if(i == nums.length-1) result[i] = prefix[i-1];
+            else result[i] = prefix[i-1] * suffix[i+1];
+        }
+
+        return result;
+    }
+
+}
