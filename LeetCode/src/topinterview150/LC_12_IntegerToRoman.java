a94b3506d3ef5c35f88d3d121a2c2209f0565eec
tothetop430 <jhuseyrer355545@gmail.com>
2023-05-30 13:16:29 +0530

    solve top interview 150 problem

--- LeetCode/src/topinterview150/LC_12_IntegerToRoman.java
+++ LeetCode/src/topinterview150/LC_12_IntegerToRoman.java
@@ -1,25 +1,8 @@
 package src.topinterview150;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class LC_12_IntegerToRoman {
 
     public String intToRoman(int num) {
-        Map<Integer, String> map = new HashMap<>();
-        map.put(1, "I");
-        map.put(4, "IV");
-        map.put(5, "V");
-        map.put(9, "IX");
-        map.put(10, "X");
-        map.put(40, "XL");
-        map.put(50, "L");
-        map.put(90, "XC");
-        map.put(100, "C");
-        map.put(400, "CD");
-        map.put(500, "D");
-        map.put(900, "CM");
-        map.put(1000, "M");
         StringBuilder sb = new StringBuilder();
         while(num != 0) {
             if(num >= 1000) {
