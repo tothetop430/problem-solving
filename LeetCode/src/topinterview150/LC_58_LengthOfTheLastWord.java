075058ebec6395184d68f399148fb4e040dc78d5
tothetop430 <jhuseyrer355545@gmail.com>
2023-05-30 13:27:35 +0530

    solve top interview 150 problem

--- LeetCode/src/topinterview150/LC_58_LengthOfTheLastWord.java
+++ LeetCode/src/topinterview150/LC_58_LengthOfTheLastWord.java
@@ -3,8 +3,15 @@ package src.topinterview150;
 public class LC_58_LengthOfTheLastWord {
 
     public int lengthOfLastWord(String s) {
-        String[] stringSplit = s.split(" ");
-        return stringSplit[stringSplit.length - 1].length();
+        int len = 0;
+        int i = s.length() - 1;
+        while(i>=0 && s.charAt(i) == ' ') {
+            i--;
+        }
+        while(i>=0 && s.charAt(i) != ' ') {
+            len++; i--;
+        }
+        return len;
     }
 
 }
