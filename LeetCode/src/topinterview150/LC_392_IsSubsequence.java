b72baadd9ffe3f11117e6511b2bf1e2744022bc2
tothetop430 <jhuseyrer355545@gmail.com>
2023-06-02 20:31:54 +0530

    solve top interview 150 problem

---
+++ LeetCode/src/topinterview150/LC_392_IsSubsequence.java
@@ -0,0 +1,18 @@
+package src.topinterview150;
+
+public class LC_392_IsSubsequence {
+
+    public boolean isSubsequence(String s, String t) {
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+        if(i > j) return false;
+
+        while(i>=0 && j>=0) {
+            if(s.charAt(i) == t.charAt(j)) i--;
+            j--;
+        }
+
+        return i < 0;
+    }
+
+}
