37dc85ed2e3b46497b34ced8645029e11d40df31
tothetop430 <jhuseyrer355545@gmail.com>
2023-06-02 20:31:50 +0530

    solve top interview 150 problem

---
+++ LeetCode/src/topinterview150/LC_125_ValidPalindrome.java
@@ -0,0 +1,34 @@
+package src.topinterview150;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC_125_ValidPalindrome {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+    }
+
+    public static boolean isPalindrome(String s) {
+        List<Character> arr = new ArrayList<>();
+        int pos = 0;
+        for(char chr: s.toCharArray()) {
+            if('a' <= chr && chr <= 'z' || 'A' <= chr && chr <= 'Z') {
+                arr.add(Character.toLowerCase(chr));
+            }
+        }
+
+        return isPalin(arr);
+    }
+
+    public static boolean isPalin(List<Character> arr) {
+        int start = 0;
+        int end = arr.size() - 1;
+        while(start < end) {
+            if(arr.get(start) != arr.get(end)) return false;
+            start++; end--;
+        }
+        return true;
+    }
+
+}
