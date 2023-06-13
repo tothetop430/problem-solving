107ffd2ae1345b0b955d897817dd77bbe93b62d3
tothetop430 <jhuseyrer355545@gmail.com>
2023-06-13 09:45:59 +0530

    solve top interview 150 problem

---
+++ LeetCode/src/topinterview150/LC_383_RansomNote.java
@@ -0,0 +1,17 @@
+package src.topinterview150;
+
+public class LC_383_RansomNote {
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] countMagLetters = new int[26];
+        for(char chr: magazine.toCharArray()) {
+            countMagLetters[chr - 'a']++;
+        }
+        for(char chr: ransomNote.toCharArray()) {
+            if(countMagLetters[chr - 'a'] == 0) return false;
+            countMagLetters[chr - 'a']--;
+        }
+        return true;
+    }
+
+}
