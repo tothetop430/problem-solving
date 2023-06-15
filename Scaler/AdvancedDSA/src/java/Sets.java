a7346cb48a378ebce1f34c3bf2a7e442693d5fa0
tothetop430 <jhuseyrer355545@gmail.com>
2023-06-15 18:18:45 +0530

    java language module class

---
+++ Scaler/AdvancedDSA/src/java/Sets.java
@@ -0,0 +1,15 @@
+package src.java;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Sets {
+
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+        set.add(null);
+        set.add(null);
+        System.out.println(set);
+    }
+
+}
