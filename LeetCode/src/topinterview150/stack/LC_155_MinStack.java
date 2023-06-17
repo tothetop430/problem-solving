5a446eb4d96fa14ae05b66c9443e340b192d89cc
tothetop430 <jhuseyrer355545@gmail.com>
2023-06-17 21:15:29 +0530

    solve top interview 150 problem

---
+++ LeetCode/src/topinterview150/stack/LC_155_MinStack.java
@@ -0,0 +1,37 @@
+package src.topinterview150.stack;
+
+import java.util.Stack;
+
+public class LC_155_MinStack {
+
+    static class MinStack {
+
+        private Stack<Integer> mainStack;
+        private Stack<Integer> minStack;
+
+        public MinStack() {
+            mainStack = new Stack<>();
+            minStack = new Stack<>();
+        }
+
+        public void push(int val) {
+            mainStack.push(val);
+            if (minStack.isEmpty()) minStack.push(val);
+            else if (minStack.peek() > val) minStack.push(val);
+            else minStack.push(minStack.peek());
+        }
+
+        public void pop() {
+            mainStack.pop();
+            minStack.pop();
+        }
+
+        public int top() {
+            return mainStack.peek();
+        }
+
+        public int getMin() {
+            return minStack.peek();
+        }
+    }
+}
