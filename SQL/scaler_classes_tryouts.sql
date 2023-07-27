2b7ce6024925517f0aae2d8f10a24fc10d109fbf
tothetop430 <jhuseyrer355545@gmail.com>
2023-07-27 13:53:08 +0530

    update scaler tryouts

---
+++ SQL/scaler_classes_tryouts.sql
@@ -0,0 +1,21 @@
+-- CRUD
+
+SELECT * FROM people WHERE NOT gender = 'M';
+
+SELECT * FROM people WHERE "name" NOT IN ('Riya', 'Reena');
+
+SELECT * FROM people WHERE "name" LIKE '%k%';
+
+SELECT * FROM people WHERE "name" IS NOT NULL;
+
+SELECT DISTINCT gender FROM people ORDER BY gender;
+
+ALTER TABLE relation 
+ADD FOREIGN KEY (cid) REFERENCES people(id);
+
+DELETE FROM people WHERE id = 6;
+
+SELECT * FROM relation;
+
+SELECT 'hello' FROM relation;
+
