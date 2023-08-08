d73e8b7e683a477a7af8e82e5f0f6c48f09b87d3
tothetop430 <jhuseyrer355545@gmail.com>
2023-08-08 11:19:15 +0530

    solve aggregate queries

---
+++ SQL/students_in_departments.sql
@@ -0,0 +1,5 @@
+select d.dept_name, count(s.student_id) student_number from
+student s right join department d
+on s.dept_id = d.dept_id
+group by d.dept_name
+order by student_number desc, d.dept_name;
